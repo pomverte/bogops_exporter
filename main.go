140fd7c46986af3508af90230dfed141a8741b65
Hong Viet Le <hvle@octo.com>
2022-01-01 19:31:27 +0100

    ze_status_code

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -16,6 +20,12 @@ var (
 		},
 		[]string{"level"},
 	)
+	teaPot = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "ze_status_code",
+			Help: "Un status code.",
+		},
+	)
 )
 
 func initbogopsGauge() {
@@ -26,8 +36,32 @@ func initbogopsGauge() {
 	bogopsGauge.WithLabelValues("Confirmed Mgr Ref").Set(1)
 }
 
+func intiStatusCode() {
+
+	rand.Seed(time.Now().UnixNano())
+	min := 200
+	max := 208
+
+	go func() {
+		for {
+			httpBinUrl := fmt.Sprintf("https://httpbin.org/status/%d", min+rand.Intn(max-min+1))
+			fmt.Println(httpBinUrl)
+			response, err := http.Get(httpBinUrl)
+			if err != nil {
+				fmt.Print(err.Error())
+				os.Exit(1)
+			}
+			teaPot.Set(float64(response.StatusCode))
+
+			time.Sleep(10 * time.Second)
+		}
+	}()
+}
+
 func main() {
 	initbogopsGauge()
+
+	intiStatusCode()
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8080", nil)
